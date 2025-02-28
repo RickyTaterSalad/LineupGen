cd571aadfc76c0dfcfa88a6d1d546e63524974f0
RickyTaterSalad <rickymyrna@gmail.com>
2025-02-28 10:03:59 -0800

    generator

---
+++ Parser.cs
@@ -0,0 +1,61 @@
+﻿
+namespace GameGenerator
+{
+	public class Parser
+	{
+		const string lineupTemplate = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><link rel=\"stylesheet\" href=\"../../../style.css\"><title>#TITLE#</title></head><body><div class=\"exported-note\"><div class=\"exported-note-title\">#TITLE#</div><div id=\"rendered-md\">#TABLES#<h1 id=\"links\"><strong>Links</strong></h1><p><a title=\"https://riverabaseball.com\" href=\"https://riverabaseball.com\">Home</a>&nbsp;&nbsp;&nbsp;<a title=\"https://www.youtube.com/playlist?list=PLdbXG0VpP0Mb4p5j_fUam-AX1btECUJNR\" href=\"https://www.youtube.com/playlist?list=PLdbXG0VpP0Mb4p5j_fUam-AX1btECUJNR\">YouTube Playlist</a>&nbsp;&nbsp;&nbsp;<a href=\"./archive/index.html\">Archived Lineups</a></p></div></div></body></html>";
+
+		public static bool WriteLineupTable(string path, string outputFolder) {
+
+			if (Directory.Exists(outputFolder))
+			{
+				var res = Parser.ParseLineupFile(path);
+				if (!string.IsNullOrWhiteSpace(res.Item1) && !string.IsNullOrWhiteSpace(res.Item2))
+				{
+					File.WriteAllText(Path.Combine(outputFolder, $"{res.Item1}.html"), res.Item2);
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static Tuple<string, string> ParseLineupFile(string path)
+		{
+			var gameTitle = string.Empty;
+			if (File.Exists(path))
+			{
+				var text = File.ReadAllLines(path);
+				var tableHtml = string.Empty;
+				int currentOrder = 1;
+				foreach (var line in text)
+				{
+					var trimmed = line.Trim();
+					if (line.StartsWith("!"))
+					{
+						gameTitle = line[1..].Trim();
+					}
+					else if (line.StartsWith("#"))
+					{
+						currentOrder = 1;
+						if (!string.IsNullOrWhiteSpace(tableHtml))
+						{
+							tableHtml += "</tbody></table>";
+						}
+						tableHtml += $"<h1 id=\"{trimmed[1..]}\"><strong>{trimmed[1..]}</strong></h1><div class=\"joplin-table-wrapper\"><table><thead><tr><th>Order</th><th>Name</th><th>Position</th></tr></thead><tbody>";
+					}
+					else
+					{
+						var split = trimmed.Split(';');
+						if (split.Length > 1)
+						{
+							tableHtml += $"<tr><td>{currentOrder++}</td><td>{split[0]}</td><td>{split[1]}</td></tr>";
+						}
+					}
+				}
+				tableHtml += "</tbody></table>";
+				return new Tuple<string,string>(gameTitle, lineupTemplate.Replace("#TITLE#", gameTitle).Replace("#TABLES#", tableHtml));
+			}
+			return new Tuple<string, string>(string.Empty, string.Empty);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GameGenerator;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+
+Console.WriteLine("1 - Current Lineup (TEXT FILE)");
+Console.WriteLine("2 - Archive Existing");
+Console.WriteLine("3 - Current Lineup (JOPLIN TABLE)");
+var cmd = string.Empty;
+var retries = 10;
+var count = 0;
+
+while (cmd != "1" && cmd != "2" && cmd != "3" && count++ < retries)
+{
+	cmd  = Console.ReadLine();
+}
+count = 0;
+if (cmd == "3")
+{
+	var joplinTextPath = string.Empty;
+	while (!File.Exists(joplinTextPath) && count++ < retries)
+	{
+		Console.WriteLine("Path To Joplin Text:");
+		joplinTextPath = Console.ReadLine();
+	}
+	if (File.Exists(joplinTextPath))
+	{
+		var tableRowRegexp = new Regex("\\|[0-9\\s]+\\| (.*) \\| (.*) \\|");
+		var title = string.Empty;
+		var joplinLines = File.ReadAllLines(joplinTextPath);
+		int curr = 0;
+		var lineupDict = new Dictionary<string, Dictionary<string, string>>();
+		var currentDict = string.Empty;
+
+		foreach (var line in joplinLines)
+		{
+			if (curr == 0)
+			{
+				title = line[1..].Trim();
+			}
+			else
+			{
+				if (string.IsNullOrWhiteSpace(line))
+				{
+					continue;
+				}
+				if (line.StartsWith("#"))
+				{
+					currentDict = line[1..].Trim();
+					if (!lineupDict.ContainsKey(currentDict))
+					{
+						lineupDict.Add(currentDict, new Dictionary<string, string>());
+					}
+				}
+				else
+				{
+					//some sort of table entry
+					if (line.Contains("---"))
+					{
+						continue;
+					}
+					if (line.Contains("Order") && line.Contains("Name") && line.Contains("Position"))
+					{
+						continue;
+					}
+					else
+					{
+						var m = tableRowRegexp.Match(line);
+						if (m.Success && m.Groups.Count > 2)
+						{
+							var player = m.Groups[1].Value.Trim();
+							var position = m.Groups[2].Value.Trim();
+							if (lineupDict.ContainsKey(currentDict))
+							{
+								lineupDict[currentDict].Add(player, position);
+
+							}
+						}
+					}
+
+				}
+			}
+			curr++;
+		}
+		//write temp file
+		var lines = new List<string>()
+		{
+			$"!{title}"
+		};
+		foreach (var kvp in lineupDict)
+		{
+			lines.Add($"#{kvp.Key}");
+			foreach (var playersKvp in kvp.Value)
+			{
+				lines.Add($"{playersKvp.Key};{playersKvp.Value}");
+			}
+		}
+		var tempFile = Path.GetTempFileName();
+		File.WriteAllLines(tempFile, lines);
+		Parser.WriteLineupTable(tempFile, Path.GetDirectoryName(joplinTextPath) ?? string.Empty);
+		File.Delete(tempFile);
+	}
+}
+else if (cmd == "1")
+{
+	var lineupTextPath = string.Empty;
+	while (!File.Exists(lineupTextPath) && count++ < retries)
+	{
+		Console.WriteLine("Path To Lineup Text:");
+		lineupTextPath = Console.ReadLine();
+	}
+	if (File.Exists(lineupTextPath))
+	{
+		Parser.WriteLineupTable(lineupTextPath, Path.GetDirectoryName(lineupTextPath) ?? string.Empty);
+	}
+}
+
+else if (cmd == "2")
+{
+	var userProvidedPath = string.Empty;
+	while (!File.Exists(userProvidedPath) && count++ < retries)
+	{
+		Console.WriteLine("Path To Existing Lineup HTML:");
+		userProvidedPath = Console.ReadLine();
+	}
+	var gameTitle = string.Empty;
+	if (File.Exists(userProvidedPath))
+	{
+		var allText = File.ReadAllText(userProvidedPath);
+		var titleRegexp = new Regex("<title>(.*)<\\/title>");
+		var title = string.Empty;
+		var m = titleRegexp.Match(allText);
+		if (m.Success && m.Groups.Count > 1)
+		{
+			title = m.Groups[1].Value;
+		}
+		allText = allText.Replace("/archive/", "/").Replace("../style.css","../../style.css");
+
+		count = 0;
+		userProvidedPath = string.Empty;
+		while (!Directory.Exists(userProvidedPath) && count++ < retries)
+		{
+			Console.WriteLine("Output Directory:");
+			userProvidedPath = Console.ReadLine();
+		}
+		if (Directory.Exists(userProvidedPath))
+		{
+			var outputFile = Path.Combine(userProvidedPath, title, ".html");
+			File.WriteAllText(outputFile, allText);
+		}
+	}
+}
