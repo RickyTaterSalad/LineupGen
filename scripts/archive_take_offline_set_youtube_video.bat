132c697b4a656f2771fa200aaa9e8902afc2ce4e
RickyTaterSalad <rickymyrna@gmail.com>
2025-03-05 12:21:00 -0800

    update

--- scripts/archive_take_offline_set_youtube_video.bat
+++ scripts/archive_take_offline_set_youtube_video.bat
@@ -8,7 +8,7 @@ set LINEUP_GEN_EXE=C:\Github\LineupGen\bin\Release\net9.0\LineupGen.exe
 
 cd "%WEBSITE_DIR%"
 git pull
-git commit -am "Sync Latest Youtube Video %DATE:~-4%_%DATE:~4,2%_%DATE:~7,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%"
+git commit -am "archive_take_offline_set_youtube_video %DATE:~-4%_%DATE:~4,2%_%DATE:~7,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%"
 git push
 
 cd "%~dp0"
