e1306cd3204856b7fc45c7549958523e6f795d11
RickyTaterSalad <rickymyrna@gmail.com>
2025-03-13 11:16:57 -0700

    add shell scripts

--- scripts/lineup_archive.sh
+++ scripts/lineup_archive.sh
@@ -3,7 +3,6 @@
 WEBSITE_DIR="/home/server/Documents/Github/BaseballWebsite"
 TEAM_ROOT_DIR="$WEBSITE_DIR/2025/Mustang/Cubs"
 LINEUP_GEN_REPO="/home/server/Documents/Github/LineupGen"
-LINEUPS_REPO="/home/server/Documents/Github/BaseballLineups"
 LINEUP_GEN_EXE="$LINEUP_GEN_REPO/bin/Release/net80/LineupGen"
 
 
@@ -13,16 +12,9 @@ cd "$WEBSITE_DIR"
 git pull
 
 # publish lineup
-$LINEUP_GEN_EXE -m publish -r "$WEBSITE_DIR" -t "$TEAM_ROOT_DIR" -d "$LINEUPS_REPO"
+$LINEUP_GEN_EXE -m archive -r "$WEBSITE_DIR" -t "$TEAM_ROOT_DIR"
 
 
 git add .
-git commit -m "Publish Lineup $timestamp"
+git commit -m "Archive Lineup $timestamp"
 git push
-
-cd "$LINEUPS_REPO"
-git pull
-git add .
-git commit -m "Publish Lineup $timestamp"
-git push
-
--- scripts/set_youtube_video.sh
+++ scripts/set_youtube_video.sh
@@ -3,7 +3,6 @@
 WEBSITE_DIR="/home/server/Documents/Github/BaseballWebsite"
 TEAM_ROOT_DIR="$WEBSITE_DIR/2025/Mustang/Cubs"
 LINEUP_GEN_REPO="/home/server/Documents/Github/LineupGen"
-LINEUPS_REPO="/home/server/Documents/Github/BaseballLineups"
 LINEUP_GEN_EXE="$LINEUP_GEN_REPO/bin/Release/net80/LineupGen"
 
 
@@ -12,17 +11,9 @@ timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 cd "$WEBSITE_DIR"
 git pull
 
-# publish lineup
-$LINEUP_GEN_EXE -m publish -r "$WEBSITE_DIR" -t "$TEAM_ROOT_DIR" -d "$LINEUPS_REPO"
+$LINEUP_GEN_EXE -m youtube -t "$TEAM_ROOT_DIR" -u "$1"
 
 
 git add .
-git commit -m "Publish Lineup $timestamp"
+git commit -m "Set YouTube $timestamp"
 git push
-
-cd "$LINEUPS_REPO"
-git pull
-git add .
-git commit -m "Publish Lineup $timestamp"
-git push
-
